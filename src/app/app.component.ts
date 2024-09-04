04dcd4499a02c621ff58e4b111d336110c136996
Sammy maina <sammymainakirathe@gmail.com>
2024-09-04 09:06:30 +0300

    Added 404 page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { SignUpComponent } from './signup/signup.component';
 import { VerifyOtpComponent } from './otp-verification/otp-verification.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SignUpComponent, VerifyOtpComponent, LoginComponent],
+  imports: [RouterOutlet, SignUpComponent, VerifyOtpComponent, LoginComponent, PageNotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from './signup/signup.component';
 import { VerifyOtpComponent } from './otp-verification/otp-verification.component';
-import { LoginComponent } from './login/login.component'; // Import the LoginComponent
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'; // Import the PageNotFoundComponent
 
 export const routes: Routes = [
   { path: '', redirectTo: 'signup', pathMatch: 'full' }, // Redirects root path to signup
   { path: 'signup', component: SignUpComponent },
   { path: 'verify-otp', component: VerifyOtpComponent },
-  { path: 'login', component: LoginComponent }, // Add route for LoginComponent
-  { path: '**', redirectTo: 'signup' } // Redirects any undefined paths to signup
+  { path: 'login', component: LoginComponent },
+  { path: '404', component: PageNotFoundComponent }, // Route for the 404 page
+  { path: '**', redirectTo: '404' } // Redirects any undefined paths to the 404 page
 ];
